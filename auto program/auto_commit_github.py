ecbe911c4c4eaea607eda5a843f75eacbd98aa64
CodingCola <138351610+CodingColaTW@users.noreply.github.com>
2024-06-17 23:11:03 +0800

    [new]default setting

---
+++ auto program/auto_commit_github.py
@@ -0,0 +1,23 @@
+import os
+import git
+import schedule
+import time
+from datetime import datetime
+
+# 設置 Git repository 的路徑
+repo_path = '/path/to/your/repo'
+
+def commit_changes():
+    repo = git.Repo(repo_path)
+    repo.git.add('--all')
+    commit_message = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    repo.index.commit(commit_message)
+    origin = repo.remote(name='origin')
+    origin.push()
+
+# 設置每天晚上11:30自動執行
+schedule.every().day.at("23:30").do(commit_changes)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
